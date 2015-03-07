d00a3881eb91bfc26462c9484af7f3f97ab7316f
Patrick Pissurno <patrickpissurno.nave@gmail.com>
2015-03-07 17:10:53 -0300

    Fixed a little bug in the control dll
    The DragBox control dll wasn't using the color selected in the Designer.
    Now everything should work fine.

---
+++ DragBox and DragLabel Controls/DragBox/DragBox.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace DragBox
+{
+    public partial class DragBox : Button
+    {
+        private int id = 0;
+        private Color color = Color.White;
+        private object linkedObject = null;
+        private bool empty = true;
+
+        private void InitializeComponent()
+        {
+        }
+
+        public DragBox()
+        {
+            base.FlatStyle = FlatStyle.Flat;
+            base.Text = "";
+        }
+
+        [Browsable(true)]
+        [Description("Sets the DragBox's ID")]
+        [DisplayName("ID")]
+        [DefaultValue(0)]
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+
+            set
+            {
+                this.id = value;
+            }
+        }
+
+        [Browsable(true)]
+        [Description("Wheter the DragBox is empty or not")]
+        [DisplayName("Empty")]
+        [DefaultValue(true)]
+        public bool Empty
+        {
+            get
+            {
+                return this.empty;
+            }
+
+            set
+            {
+                this.empty = value;
+            }
+        }
+
+        [Browsable(true)]
+        [Description("Sets the DragBox's Color")]
+        [DisplayName("Color")]
+        public Color Color
+        {
+            get
+            {
+                return this.color;
+            }
+
+            set
+            {
+                this.color = value;
+                base.BackColor = value;
+                base.FlatAppearance.BorderColor = value;
+                base.FlatAppearance.MouseDownBackColor = value;
+                base.FlatAppearance.MouseOverBackColor = value;
+                Invalidate();
+            }
+        }
+
+        [Browsable(true)]
+        [Description("Sets the DragBox's Linked Object")]
+        [DisplayName("Linked Object")]
+        [DefaultValue(null)]
+        public object LinkedObject
+        {
+            get
+            {
+                return this.linkedObject;
+            }
+
+            set
+            {
+                this.linkedObject = value;
+            }
+        }
+    }
+}
---
+++ DragBox and DragLabel Controls/DragBox/DragLabel.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.ComponentModel;
+
+namespace DragBox
+{
+    public class DragLabel : Label
+    {
+        private object linkedObject;
+        private Color normalColor;
+        private Color linkedColor;
+        private bool linked = false;
+        private Point lastLocation;
+
+        public DragLabel()
+        {
+            this.BackColor = Color.Transparent;
+            base.TextAlign = ContentAlignment.MiddleCenter;
+        }
+
+        [Browsable(true)]
+        [Description("Sets the DragBox's Linked Object")]
+        [DisplayName("Linked Object")]
+        [DefaultValue(null)]
+        public object LinkedObject
+        {
+            get
+            {
+                return this.linkedObject;
+            }
+            set
+            {
+                this.linkedObject = value;
+            }
+        }
+
+        [Browsable(true)]
+        [Description("Whether the DragLabel is linked or not")]
+        [DisplayName("Linked")]
+        [DefaultValue(false)]
+        public bool Linked
+        {
+            get
+            {
+                return this.linked;
+            }
+            set
+            {
+                this.linked = value;
+                if (!value)
+                {
+                    this.LinkedObject = null;
+                    this.BackColor = this.NormalColor;
+                    Invalidate();
+                }
+                else
+                {
+                    this.BackColor = this.LinkedColor;
+                    this.LastLocation = this.Location;
+                    Invalidate();
+                }
+            }
+        }
+
+        [Browsable(true)]
+        [Description("DragLabel's color when linked to a DragBox")]
+        [DisplayName("Linked Color")]
+        public Color LinkedColor
+        {
+            get
+            {
+                return this.linkedColor;
+            }
+            set
+            {
+                this.linkedColor = value;
+            }
+        }
+
+        [Browsable(true)]
+        [Description("DragLabel's color when not linked to a DragBox")]
+        [DisplayName("Normal Color")]
+        public Color NormalColor
+        {
+            get
+            {
+                return this.normalColor;
+            }
+            set
+            {
+                this.normalColor = value;
+            }
+        }
+
+        [Browsable(false)]
+        [Description("Last Location Point")]
+        [DisplayName("Last Location")]
+        public Point LastLocation
+        {
+            get
+            {
+                return this.lastLocation;
+            }
+            set
+            {
+                this.lastLocation = value;
+            }
+        }
+    }
+}
